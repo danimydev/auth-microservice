d6892401efad276b962556b76c047c2b1413c2fa
Daniel Madrid <danimydev@gmail.com>
2022-08-17 20:30:07 -0500

    add envconfig types

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,19 @@
 require('dotenv').config();
 
-export default {
-  port: process.env.PORT,
-  host: process.env.HOST,
+type EnvConfig = {
+  port: number,
+  host: string,
   jwt: {
-    key: process.env.JWT_KEY,
-    defaultExp: process.env.JWT_DEFAULT_EXP,
+    key: string,
   },
-  oauth: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      redirectUrl: `${process.env.HOST}:${process.env.PORT}/oauth/github/cb`,
-      baseUrl: process.env.GITHUB_OAUTH_BASE_URL,
-    }
-  }
 }
+
+const envConfig: EnvConfig = {
+  port: Number(process.env.PORT),
+  host: process.env.HOST || 'http://localhost',
+  jwt: {
+    key: process.env.JWT_KEY || 'secret',
+  },
+}
+
+export default envConfig;
