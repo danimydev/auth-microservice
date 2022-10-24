37f2768c4263641de8ac71790012a9f4efa6b843
Daniel Madrid <danimydev@gmail.com>
2022-10-24 03:49:55 -0500

    add redirect and controller adapter

--- src/web/adapter.ts
+++ src/web/adapter.ts
@@ -1,13 +1,14 @@
-import {  Request, Response } from "express";
+import { Request, Response } from "express";
 import { HttpController, HttpRequest, HttpResponse, HttpStatusCodes } from "./types";
 
-function adaptController(controller: HttpController) {
+function controllerAdapter(controller: HttpController) {
   return async (req: Request, res: Response) => {
     try {
       const httpRequest: HttpRequest = {
         headers: req.headers,
         params: req.params,
         body: req.body,
+        query: req.query,
       }
       const httpResponse: HttpResponse = await controller.execute(httpRequest);
       res.status(httpResponse.statusCode).json(httpResponse.body);
@@ -19,4 +20,19 @@ function adaptController(controller: HttpController) {
   }
 }
 
-export default adaptController;
+function redirectAdapter(redirectUrl: string) {
+  return async (req: Request, res: Response) => {
+    try {
+      res.redirect(redirectUrl);
+    } catch (error: any) {
+      res.status(HttpStatusCodes.INTERNAL_ERROR).json({
+        error: error.message
+      });
+    }
+  }
+}
+
+export {
+  controllerAdapter,
+  redirectAdapter,
+};
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from 'express';
 import { signController, verifyController } from '../jwt';
-import adaptController from './adapter';
-import envConfig from '../config';
-import GitHubExpressControllers from '../github/controllers';
+import { controllerAdapter, redirectAdapter } from './adapter';
+import { callbackController, loginAuthUrl } from '../github';
 
 const app = express();
 
@@ -12,11 +11,10 @@ app.use('/health', (req: Request, res: Response): void => {
   res.status(200).json('Hello World');
 });
 
-app.post('/jwt/sign', adaptController(signController));
-app.post('/jwt/verify', adaptController(verifyController));
+app.post('/jwt/sign', controllerAdapter(signController));
+app.post('/jwt/verify', controllerAdapter(verifyController));
 
-app.get('/oauth/github/login', GitHubExpressControllers.expressRedirect);
-
-app.get(new URL(envConfig.github.redirectUrl).pathname, GitHubExpressControllers.redirectCallBackHandler);
+app.get('/oauth/github/login', redirectAdapter(loginAuthUrl));
+app.get(callbackController.authCallBackRoute, controllerAdapter(callbackController));
 
 export default app;
