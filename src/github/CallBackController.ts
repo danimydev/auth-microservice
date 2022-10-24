b9c5568ec8f03bbd9be6de1577ebb094ac5acda8
Daniel Madrid <danimydev@gmail.com>
2022-10-24 03:49:24 -0500

    callback controller github

---
+++ src/github/CallBackController.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import envConfig from "../config";
+import { HttpController, HttpRequest, HttpResponse, HttpStatusCodes } from "../web/types";
+
+class CallBackController implements HttpController {
+
+  public readonly authCallBackRoute = new URL(envConfig.github.redirectUrl).pathname;
+
+  constructor() {
+
+  }
+
+  async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { code } = httpRequest.query;
+      if (!code) {
+        return {
+          statusCode: HttpStatusCodes.BAD_REQUEST,
+          body: 'No code passed by github',
+        }
+      }
+      const data = await this.getAccessToken(code);
+      return {
+        statusCode: HttpStatusCodes.OK,
+        body: data,
+      }
+    } catch (error) {
+      return {
+        statusCode: HttpStatusCodes.INTERNAL_ERROR,
+        body: error,
+      }
+    }
+  }
+
+  private async getAccessToken(code: string) {
+    const requestBody = {
+      client_id: envConfig.github.clientId,
+      client_secret: envConfig.github.clientSecret,
+      code,
+      redirect_uri: envConfig.github.redirectUrl,
+    };
+    const opts = {
+      headers: {
+        accept: 'application/json',
+      },
+    }
+    const rawResponse = await axios.post('https://github.com/login/oauth/access_token', requestBody, opts);
+    console.log(rawResponse.data);
+    return rawResponse.data;
+  }
+}
+
+export default CallBackController;
