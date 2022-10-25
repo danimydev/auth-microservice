72bcfd810628db09235b7b863a662088e8d368ec
Daniel Madrid <danimydev@gmail.com>
2022-10-25 10:11:03 -0500

    fix github cb controller import

--- src/github/index.ts
+++ src/github/index.ts
@@ -1,5 +1,5 @@
 import envConfig from "../config";
-import CallBackController from "./callbackController";
+import CallBackController from "./CallBackController";
 
 const callbackController = new CallBackController();
 const loginAuthUrl = `https://github.com/login/oauth/authorize?client_id=${envConfig.github.clientId}`;
